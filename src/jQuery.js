f029e20ec1f6ec88c6bf9018a0b9680a37cc6780
Julian Pollak <poljpocket@gmail.com>
2021-12-06 02:30:43 +0100

    remove jquery runtime dependency

--- src/jQuery.js
+++ src/jQuery.js
@@ -3,51 +3,43 @@ const EventfrogEventRequest = require("./request/EventfrogEventRequest");
 const EventfrogLocationRequest = require("./request/EventfrogLocationRequest");
 const EventfrogGroupRequest = require("./request/EventfrogGroupRequest");
 
+const $ = require('jquery');
+
 /**
- * EventfrogLoader jQuery plugin
- *
- * @see EventfrogService
- *
- * @author Julian Pollak <poljpocket@gmail.com>
- *
+ * @param {string} key
+ * @return {EventfrogService}
  */
-(function($) {
-    /**
-     * @param {string} key
-     * @return {EventfrogService}
-     */
-    const jQueryEventfrogService = function(key) {
-        return new EventfrogService(key);
-    }
+const jQueryEventfrogService = function (key) {
+    return new EventfrogService(key);
+}
 
-    /**
-     * @param {EventfrogEventRequestOptions} options
-     * @return {EventfrogEventRequest}
-     */
-    const jQueryEventfrogEventRequest = function(options) {
-        return new EventfrogEventRequest(options);
-    }
+/**
+ * @param {EventfrogEventRequestOptions} options
+ * @return {EventfrogEventRequest}
+ */
+const jQueryEventfrogEventRequest = function (options) {
+    return new EventfrogEventRequest(options);
+}
 
-    /**
-     * @param {EventfrogLocationRequestOptions} options
-     * @return {EventfrogLocationRequest}
-     */
-    const jQueryEventfrogLocationRequest = function(options) {
-        return new EventfrogLocationRequest(options);
-    }
+/**
+ * @param {EventfrogLocationRequestOptions} options
+ * @return {EventfrogLocationRequest}
+ */
+const jQueryEventfrogLocationRequest = function (options) {
+    return new EventfrogLocationRequest(options);
+}
 
-    /**
-     * @param {EventfrogGroupRequestOptions} options
-     * @return {EventfrogGroupRequest}
-     */
-    const jQueryEventfrogGroupRequest = function(options) {
-        return new EventfrogGroupRequest(options);
-    }
+/**
+ * @param {EventfrogGroupRequestOptions} options
+ * @return {EventfrogGroupRequest}
+ */
+const jQueryEventfrogGroupRequest = function (options) {
+    return new EventfrogGroupRequest(options);
+}
 
-    $.extend({
-        EventfrogService: jQueryEventfrogService,
-        EventfrogEventRequest: jQueryEventfrogEventRequest,
-        EventfrogGroupRequest: jQueryEventfrogGroupRequest,
-        EventfrogLocationRequest: jQueryEventfrogLocationRequest,
-    });
-})(jQuery);
+$.extend({
+    EventfrogService: jQueryEventfrogService,
+    EventfrogEventRequest: jQueryEventfrogEventRequest,
+    EventfrogGroupRequest: jQueryEventfrogGroupRequest,
+    EventfrogLocationRequest: jQueryEventfrogLocationRequest,
+});
