3f1f62016b7184d36533c4ff9a2c6ab9f4229b03
Julian Pollak <j.pollak@fruitcake.ch>
2023-02-16 17:22:39 +0100

    switch to ES6 module strategy add main file which exports all components

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+export {EventfrogService} from "./service/EventfrogService";
+
+export {EventfrogRequest} from "./request/EventfrogRequest";
+export {EventfrogEventRequest} from "./request/EventfrogRequest";
+export {EventfrogLocationRequest} from "./request/EventfrogRequest";
+export {EventfrogGroupRequest} from "./request/EventfrogRequest";
+
+export {EventfrogResult} from "./result/EventfrogResult";
+export {EventfrogEventResult} from "./result/EventfrogEventResult";
+export {EventfrogLocationResult} from "./result/EventfrogLocationResult";
+export {EventfrogGroupResult} from "./result/EventfrogGroupResult";
+
+export {EventfrogModel} from "./model/EventfrogModel";
+export {EventfrogEvent} from "./model/EventfrogEvent";
+export {EventfrogLocation} from "./model/EventfrogLocation";
+export {EventfrogGroup} from "./model/EventfrogGroup";
+export {EventfrogTopic} from "./model/EventfrogTopic";
